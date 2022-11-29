ef720fa9927bbd06e9e350fb1b8697d0b1ddd14a
Liskomes <saku.tenkula@centria.fi>
2022-11-29 15:09:42 +0200

    AI siivous

--- ai.cpp
+++ ai.cpp
@@ -13,30 +13,30 @@ AI::AI()
 bool AI::NegPosCalc(int tavoite, int nykyinen, int total, int card, float average)
 {
     average *=(5-total)-1;
-    if (average > 6.5) average*=0.5*this->risk;
-    float tavoite1 = tavoite-average;
-    float tavoite2 = tavoite+average;
-    float vaihtoehdot[4];
-    float plus = nykyinen + card;
-    float miinus = nykyinen - card;
-    vaihtoehdot[0] = tavoite1-miinus; if (vaihtoehdot[0] < 0) vaihtoehdot[0]*=-1;//MIINUS
-    vaihtoehdot[1] = tavoite2-miinus; if (vaihtoehdot[1] < 0) vaihtoehdot[1]*=-1;//MIINUS
-    vaihtoehdot[2] = tavoite1-plus; if (vaihtoehdot[2] < 0) vaihtoehdot[2]*=-1;//PLUS
-    vaihtoehdot[3] = tavoite2-plus; if (vaihtoehdot[3] < 0) vaihtoehdot[3]*=-1;//PLUS
+    if (average > 6.5) average*=0.8*this->risk;
+
+    this->tavoite1 = tavoite-average;
+    this->tavoite2 = tavoite+average;
+    this->plus = nykyinen + card;
+    this->miinus = nykyinen - card;
+    this->vaihtoehdot[0] = this->tavoite1-this->miinus; if (this->vaihtoehdot[0] < 0) this->vaihtoehdot[0]*=-1;//MIINUS
+    this->vaihtoehdot[1] = this->tavoite2-this->miinus; if (this->vaihtoehdot[1] < 0) this->vaihtoehdot[1]*=-1;//MIINUS
+    this->vaihtoehdot[2] = this->tavoite1-this->plus; if (this->vaihtoehdot[2] < 0) this->vaihtoehdot[2]*=-1;//PLUS
+    this->vaihtoehdot[3] = this->tavoite2-this->plus; if (this->vaihtoehdot[3] < 0) this->vaihtoehdot[3]*=-1;//PLUS
 
     float index = 1000;
 
     for(int i = 0; i < 4; i++)
     {
-        if ((index > vaihtoehdot[i]))
+        if ((index > this->vaihtoehdot[i]))
         {
-            index = vaihtoehdot[i];
+            index = this->vaihtoehdot[i];
         }
     }
 
     for(int i = 0; i < 4; i++)
     {
-        if (index == vaihtoehdot[i])
+        if (index == this->vaihtoehdot[i])
         {
             index = i;
             break;
--- ai.h
+++ ai.h
@@ -7,6 +7,11 @@ class AI
 private:
     double risk;
     double risk_g;
+    float vaihtoehdot[4];
+    float tavoite1;
+    float tavoite2;
+    float plus;
+    float miinus;
 public:
     AI();
     bool NegPosCalc(int tavoite, int nykyinen, int total, int card, float average);
